7defbe27baeb29fe8d5f336ff71a7004a057dcbd
Jonathan Grah <theflametrooper@gmail.com>
2018-12-23 01:09:17 +0000

    Added raycasting based on pointer right click
    Signed-off-by: Jonathan Grah <theflametrooper@gmail.com>

---
+++ container/Assets/Scripts/Raycast.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Raycast : MonoBehaviour
+{
+    private Vector3 targetPosition; // target position that player will move to
+
+    NavMeshAgent playerAgent;
+
+
+    void Awake()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(1)) // right click
+        {
+            SetTargetPosition();
+        }
+
+        MovePlayer();
+
+    }
+
+    void SetTargetPosition()
+    {
+        Plane plane = new Plane(Vector3.up, transform.position);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float point = 0f;
+
+        if (plane.Raycast(ray, out point))
+        {
+            targetPosition = ray.GetPoint(point);
+        }
+    }
+
+    void MovePlayer()
+    {
+        playerAgent.SetDestination(targetPosition);
+        Debug.DrawLine(transform.position, targetPosition, Color.blue); // debugging line
+    }
+
+
+
+}
