2445affdc6b955170858918110fd834cb1b55142
Jonathan Grah <theflametrooper@gmail.com>
2018-12-24 05:02:54 +0000

    added multiple unit selection; movement not implemented yet based on selection
    Signed-off-by: Jonathan Grah <theflametrooper@gmail.com>

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    bool isSelecting = false;
+    Vector3 initialMousePosition;
+
+    public GameObject selectionCirclePrefab;
+
+    public bool IsWithinSelectionBounds(GameObject gameObject)
+    {
+        if (!isSelecting)
+            return false;
+
+        var camera = Camera.main;
+        var viewportBounds =
+            Utils.GetViewportBounds(camera, initialMousePosition, Input.mousePosition);
+
+        return viewportBounds.Contains(
+            camera.WorldToViewportPoint(gameObject.transform.position));
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // when click is pressed down
+        {
+            isSelecting = true;
+            initialMousePosition = Input.mousePosition;
+
+            foreach (var selectableUnit in FindObjectsOfType<SelectableUnit>())
+            {
+                if (selectableUnit.selectionCircle != null)
+                {
+                    Destroy(selectableUnit.selectionCircle.gameObject);
+                    selectableUnit.selectionCircle = null;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0)) // when click is released
+        {
+            isSelecting = false;
+        }
+
+        if (isSelecting)
+        {
+            foreach (var selectableUnit in FindObjectsOfType<SelectableUnit>())
+            {
+                if (IsWithinSelectionBounds(selectableUnit.gameObject))
+                {
+                    if (selectableUnit.selectionCircle == null)
+                    {
+                        selectableUnit.selectionCircle = Instantiate(selectionCirclePrefab);
+                        selectableUnit.selectionCircle.transform.SetParent(selectableUnit.transform, false);
+                        selectableUnit.selectionCircle.transform.eulerAngles = new Vector3(90, 0, 0);
+
+                    }
+                }
+                else
+                {
+                    if (selectableUnit.selectionCircle != null)
+                    {
+                        Destroy(selectableUnit.selectionCircle.gameObject);
+                        selectableUnit.selectionCircle = null; 
+                    }
+                }
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if (isSelecting)
+        {
+            var rect = Utils.GetScreenRect(initialMousePosition, Input.mousePosition);
+            Utils.DrawScreenRectBorder(rect, 1, Color.black);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Utils
+{
+    static Texture2D _texture;
+    public static Texture2D Texture
+    {
+        get
+        {
+            if (_texture == null) // ensure only one
+            {
+                _texture = new Texture2D(1, 1);
+                _texture.SetPixel(0, 0, Color.white);
+                _texture.Apply();
+            }
+
+            return _texture;
+        }
+    }
+
+    public static void DrawScreenRect(Rect rect, Color color)
+    {
+        GUI.color = color;
+        GUI.DrawTexture(rect, Texture);
+        GUI.color = Color.white;
+    }
+
+    public static void DrawScreenRectBorder(Rect rect, float thickness, Color color)
+    {
+        Utils.DrawScreenRect(new Rect(rect.xMin, rect.yMin, rect.width, thickness), color); // top
+        Utils.DrawScreenRect(new Rect(rect.xMin, rect.yMin, thickness, rect.height), color); // left
+        Utils.DrawScreenRect(new Rect(rect.xMax - thickness, rect.yMin, thickness, rect.height), color); // right
+        Utils.DrawScreenRect(new Rect(rect.xMin, rect.yMax - thickness, rect.width, thickness), color); // bottom
+    }
+
+    public static Rect GetScreenRect(Vector3 initialPosition, Vector3 newPosition)
+    {
+        // move origin from bottom left to top left
+        initialPosition.y = Screen.height - initialPosition.y;
+        newPosition.y = Screen.height - newPosition.y;
+        // corners
+        var topLeft = Vector3.Min(initialPosition, newPosition);
+        var bottomRight = Vector3.Max(initialPosition, newPosition);
+
+        return Rect.MinMaxRect(topLeft.x, topLeft.y, bottomRight.x, bottomRight.y);
+    }
+
+    public static Bounds GetViewportBounds(Camera camera, Vector3 initialPosition, Vector3 newPosition)
+    {
+        var v1 = Camera.main.ScreenToViewportPoint(initialPosition);
+        var v2 = Camera.main.ScreenToViewportPoint(newPosition);
+        var min = Vector3.Min(v1, v2); var max = Vector3.Max(v1, v2);
+        min.z = camera.nearClipPlane; max.z = camera.farClipPlane;
+        var bounds = new Bounds();
+        bounds.SetMinMax(min, max);
+        return bounds;
+    }
+}
