7c2a29d50de6b678d68b562a777c9d039f31be3a
Dytoa <46061079+Dytoa@users.noreply.github.com>
2018-12-25 16:00:29 +0000

    Created basic spawning script and HQ spawn point

---
+++ Assets/Scripts/SpawnOnClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnClick : MonoBehaviour
+{
+
+    public GameObject tank;
+    public GameObject unit2;
+
+    public GameObject spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnUnitOne() //called by button 1 to create unit1 at the HQ spawn point
+    {
+        Instantiate(tank, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+
+    public void SpawnUnitTwo() //called by button 2 to create unit2 at the HQ spawn point
+    {
+        Instantiate(unit2, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+}
