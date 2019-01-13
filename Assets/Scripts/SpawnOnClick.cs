ab28387d49dee6934d50d16af32ad0ed845c77cf
Dytoa <46061079+Dytoa@users.noreply.github.com>
2019-01-13 05:51:07 +0000

    Added spawn point

--- Assets/Scripts/SpawnOnClick.cs
+++ Assets/Scripts/SpawnOnClick.cs
@@ -6,9 +6,20 @@ public class SpawnOnClick : MonoBehaviour
 {
 
     public GameObject tank;
+    public float unitOneSpawnCooldown;
+
     public GameObject unit2;
+    public float unitTwoSpawnCooldown;
+
+    public GameObject unit3;
+    public float unitThreeSpawnCooldown;
 
     public GameObject spawnPoint;
+
+
+    public bool CooldownActive;
+    public float timer;
+
 
     public void SpawnUnitOne() // called by button 1 to create unit1 at the HQ spawn point
     {
@@ -19,4 +30,10 @@ public class SpawnOnClick : MonoBehaviour
     {
         Instantiate(unit2, spawnPoint.transform.position, spawnPoint.transform.rotation);
     }
+
+    public void SpawnUnitThree() // called by button 3 to create unit3 at the HQ spawn point
+    {
+        Instantiate(unit3, spawnPoint.transform.position, spawnPoint.transform.rotation);
+    }
+
 }
