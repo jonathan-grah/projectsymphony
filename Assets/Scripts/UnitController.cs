8edfae60fcb1de3e2b4797fb7c941bc169f986d8
Jonathan Grah <theflametrooper@gmail.com>
2019-01-21 22:49:43 +0000

    pathfinding doesn't allowing for clipping anymore
    Signed-off-by: Jonathan Grah <theflametrooper@gmail.com>

---
+++ Assets/Scripts/UnitController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent playerAgent;
+
+    void Awake()
+    {
+        playerAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (gameObject.GetComponent<SelectableUnit>().selectionCircle != null)
+        {
+            if (Input.GetMouseButtonDown(1)) // right click
+                move();
+        }
+    }
+
+    void move()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+        {
+            playerAgent.destination = hit.point;
+        }
+    }
+}
