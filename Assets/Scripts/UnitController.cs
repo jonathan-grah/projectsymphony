b2d31bbb00e70d8696f0bedf98ef47f10e357627
Jonathan Grah <theflametrooper@gmail.com>
2019-02-03 21:13:49 +0000

    Allowed for more manual control of navmeshagent movement
    Signed-off-by: Jonathan Grah <theflametrooper@gmail.com>

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class UnitController : MonoBehaviour
 {
     public UnitDetails unitDetails;
+    NavMeshPath path;
 
-    UnityEngine.AI.NavMeshAgent playerAgent;
+    NavMeshAgent unitAgent;
+    public int currentCorner = 0;
+
 
     void Awake()
     {
-        playerAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        unitAgent = GetComponent<NavMeshAgent>();
     }
 
     public void Save()
@@ -34,6 +38,14 @@ public class UnitController : MonoBehaviour
         if (gameObject.GetComponent<SelectableUnit>().selectionCircle != null)
             if (Input.GetMouseButtonDown(1)) // right click
                 move();
+        if (path != null && path.corners.Length > currentCorner + 1)
+            if (Vector3.Distance(transform.position, path.corners[currentCorner]) <= 0.5f)
+            {
+                currentCorner += 1;
+                transform.LookAt(path.corners[currentCorner]);
+                if (Vector3.Dot(transform.forward, (path.corners[currentCorner] - transform.position).normalized) > 0.9)
+                    unitAgent.destination = path.corners[currentCorner];
+            }
     }
 
     void move()
@@ -41,6 +53,10 @@ public class UnitController : MonoBehaviour
         RaycastHit hit;
 
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
-            playerAgent.destination = hit.point;
+        {
+            path = new NavMeshPath();
+            currentCorner = 0;
+            unitAgent.CalculatePath(hit.point, path);
+        }
     }
 }
